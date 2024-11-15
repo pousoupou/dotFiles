a11bdb091ddf0364479ebede1a40dc26960b6dd2
pousoupou <stelios.papaspiros@gmail.com>
2024-11-15 16:54:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,33 @@
 #!bin/bash
 
+# get system os
+os=$(uname -s)
+
+# if os = macos install brew
+if [ $os = "Darwin" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install stow
+    brew install neovim
+fi
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# if os = fedora install neovim
+if [ $os = "Linux" ]; then
+    sudo dnf install -y neovim python3-neovim
+    sudo dnf install stow
+
+fi
+
+# install nvchad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
