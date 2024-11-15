07ce923a37158e423d576b68a6af9b4128dac9e5
pousoupou <stelios.papaspiros@gmail.com>
2024-11-15 20:10:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,27 +17,35 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # specific to each OS
 
-# if os = macos install brew
+# if os = macos
 if [ $os = "Darwin" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install font-comic-shanns-mono-nerd-font
     brew install stow
+    brew install ripgrep
+    brew install fzf
+    brew install fd
     brew install neovim
 fi
 
-# if os = fedora install neovim
+# if os = fedora
 if [ $os = "Linux" ]; then
-    sudo dnf install -y neovim python3-neovim
     sudo dnf install stow
+    sudo dnf install ripgrep
+    sudo dnf install fzf
+    sudo dnf install fd-find
+    sudo dnf install -y neovim python3-neovim
 
+    # install font
+    git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
+    cd nerd-fonts
+    ./install.sh "ComicShannsMono Nerd Font"
+    cd ..
+
+    
 fi
 
 # install nvchad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
-# for every directory in dotfiles
-for dir in $(ls dotfiles); do
-    # if directory is not . or ..
-    if [ $dir != "." ] && [ $dir != ".." ]; then 
-        
-    fi
-done
+
